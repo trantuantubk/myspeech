973b2762e74c51aa5dc56550526888bb5357cf54
trantuantubk <trantuantubk@gmail.com>
2023-10-09 16:45:22 -0400

    Add AudioRecorder module

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AudioRecorder from './AudioRecorder';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //
+    return (
+        <>
+            <h1>
+                My Speech        
+            </h1>
+        </>
+    )
 }
-
 export default App;
