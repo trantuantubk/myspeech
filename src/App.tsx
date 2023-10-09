64f19708dc69b3892afd2918cf670b7a30a288ea
trantuantubk <trantuantubk@gmail.com>
2023-10-09 17:41:17 -0400

    display audio recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
 import AudioRecorder from './AudioRecorder';
+import { useState } from "react"
 
 function App() {
+    const [recordOption, setRecordOption] = useState("audio");
     //
     return (
         <>
             <h1>
                 My Speech        
             </h1>
+            <AudioRecorder />
         </>
     )
 }
