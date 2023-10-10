30be77ad9eaf7c2db7290a7fed35e9895071f498
trantuantubk <trantuantubk@gmail.com>
2023-10-09 23:26:50 -0400

    AudioRecorder: still error

--- src/AudioRecorder.tsx
+++ src/AudioRecorder.tsx
@@ -1,7 +1,14 @@
 import { useState, useRef } from "react";
+
+const mimeType = "audio/webm";
+
 const AudioRecorder = () =>  {
     const [permission, setPermission] = useState(false);
-    const [stream, setStream] = useState<MediaStream>();
+    const mediaRecorder = useRef<MediaStream | null>(null);
+    const [recordingStatus, setRecordingStatus] = useState('inactive');
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+    const [audio, setAudio] = useState(null)
 
     const getMicrophonePermission = async() => {
         if ("MediaRecorder" in window) {
@@ -20,6 +27,31 @@ const AudioRecorder = () =>  {
         } else {
             alert("The MediaRecorder API is not support in your browser")
         }
+    };
+
+    const startRecording = async() => {
+        setRecordingStatus("recording");
+        //create new Media recorder instance using the stream
+        const media = new MediaRecorder(stream);
+        //set the MediaRecorder instance to the mediaRecorder ref
+        if(mediaRecorder.current == null) {
+            mediaRecorder.current = media;
+        }
+        
+        mediaRecorder.current.start();
+        let localAudioChunks : Blob[] = [];
+        mediaRecorder.ondataavailable = (event) => {
+            if(typeof event.data === "undefined") return;
+            localAudioChunks.push(event.data);
+        };
+        setAudioChunks(localAudioChunks);
+    };
+
+    const stopRecording = () => {
+        setRecordingStatus("inactive");
+        //stops the recording instance
+        mediaRecorder.stop();
+        mediaRecorder.onStop();
     };
     return (
         <div>
